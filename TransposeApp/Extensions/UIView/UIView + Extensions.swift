41f8972e627b5eca85910723ced81f26ac5f888d
Janco Erasmus <jancoera@gmail.com>
2021-03-11 22:26:06 +0200

    Custom Note View Created

--- TransposeApp/Extensions/UIView/UIView + Extensions.swift
+++ TransposeApp/Extensions/UIView/UIView + Extensions.swift
@@ -11,8 +11,31 @@ import UIKit
 extension UIView {
     
     func addShadow() {
-        self.layer.shadowColor = UIColor.gray.cgColor
-        self.layer.shadowRadius = 10
-        self.layer.shadowOpacity = 0.8
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = .zero//CGSize(width: 0.5, height: 0.5)
+        self.layer.shadowRadius = 5
+        self.layer.shadowOpacity = 1
+    }
+}
+
+internal extension UIView {
+    func setupNib() {
+        guard let nibView = viewFromNibClass() else { return }
+        nibView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        self.addSubview(nibView)
+        nibView.frame = self.bounds
+    }
+    
+    func viewFromNibClass() -> UIView? {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: self.className, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+}
+
+public extension NSObjectProtocol {
+    
+    var className: String {
+        return String(describing: type(of: self))
     }
 }
---
+++ TransposeApp/Reuseable Components/Transposed Note View/TransposedNoteView.swift
@@ -0,0 +1,73 @@
+//
+//  TransposedNoteView.swift
+//  TransposeApp
+//
+//  Created by Janco Erasmus on 2021/03/11.
+//  Copyright © 2021 Janco Erasmus. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class TransposedNoteView: UIView {
+    
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var stackView: UIStackView!
+    
+    @IBOutlet weak var noteNumberLabel: UILabel!
+    @IBOutlet weak var noteFromLabel: UILabel!
+    @IBOutlet weak var noteToLabel: UILabel!
+    
+    //MARK: - IBInspectables
+    
+    @IBInspectable private var cornerRadius: CGFloat = 15 {
+        didSet {
+            configureView()
+        }
+    }
+    
+    // MARK: - Setup
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        configureView()
+    }
+    
+    // MARK: - Methods
+    
+    func populateNoteView(noteNumber: String, noteFrom: String, noteTo: String) {
+        noteNumberLabel.text = noteNumber
+        noteFromLabel.text = noteFrom
+        noteToLabel.text = noteTo
+    }
+    
+    // MARK: - Private
+    
+    private func setupView() {
+        self.setupNib()
+    }
+    
+    private func configureView() {
+
+        backgroundColor = .clear
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = .zero
+        layer.shadowOpacity = 0.2
+        layer.shadowRadius = 3
+        
+        contentView.layer.cornerRadius = cornerRadius
+        contentView.layer.masksToBounds = true
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
