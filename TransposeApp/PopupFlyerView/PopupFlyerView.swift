5161a12c45a47c71127a0aa129c60879a19774fe
Janco Erasmus <>
2021-06-30 21:54:37 +0200

    In App Purchases completed

---
+++ TransposeApp/PopupFlyerView/PopupFlyerView.swift
@@ -0,0 +1,21 @@
+//
+//  PopupFlyerView.swift
+//  TransposeApp
+//
+//  Created by Janco Erasmus on 2021/06/30.
+//  Copyright © 2021 Janco Erasmus. All rights reserved.
+//
+
+import UIKit
+
+class PopupFlyerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
